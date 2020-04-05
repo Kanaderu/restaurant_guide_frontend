acc60f41ad9efdca84e811a394316d9fa78a04ce
David Fan <fand.kanade@gmail.com>
2020-04-05 11:56:39 -0400

    Merge master (#3)
    * Make full Restaurant component title header a link
    
    * Build Google Maps clickable directions link
    
    * Adjust card overall UI & alignment
    
    - Left align for easier reading
    - Adjust font-sizing and weight of lower-heirarchy data
    - Make presence of links clearer
    - Give all links simple hover effect
    - Add text context to menu link
    
    * Make sure contact & location data stacks
    
    * Move “Status” below essential contact data
    
    * Improve Link UI +  Replace “Out” icon
    
    - Only underline right side of contact info links
    - remove “Out” icon and replace with “angle” svg icon
    - Add svg “angle” icon to all links
    - Improve markup with `p` tags over `span` where possible
    
    * Reduce Status Font Size
    
    * Removed public folder. Updated gitignore
    
    Co-authored-by: Chris Hahn <chris+digital@chrishahn.me>
    Co-authored-by: kwiromeo <kwiromeo@gmail.com>

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,36 +1,45 @@
-import React, { useState, useRef, useEffect, createContext, useContext }  from 'react';
-import mapboxgl from 'mapbox-gl';
-import PropsType from 'prop-types';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import '../css/map.css';
+import React, {
+  useState,
+  useRef,
+  useEffect,
+  createContext,
+  useContext
+} from "react";
+import mapboxgl from "mapbox-gl";
+import PropsType from "prop-types";
+import "mapbox-gl/dist/mapbox-gl.css";
+import "../css/map.css";
 
 mapboxgl.accessToken = process.env.GATSBY_MAPBOX_API_TOKEN;
 
 const MapContext = createContext(undefined);
 
-const Map = (props) => {
+const Map = props => {
   const [map, setMap] = useState(null);
   const mapContainer = useRef(null);
 
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v9',
+      style: "mapbox://styles/mapbox/streets-v9",
       center: props.center,
       zoom: props.zoom
     });
 
-    map.on('load', () => {
+    map.on("load", () => {
       map.resize();
       setMap(map);
     });
   }, []);
 
   return (
-    <div ref={mapContainer} className='map'>
-      {map != null &&
-        <MapContext.Provider value={map}> {props.children} </MapContext.Provider>
-      }
+    <div ref={mapContainer} className="map rounded-md shadow-md">
+      {map != null && (
+        <MapContext.Provider value={map}>
+          {" "}
+          {props.children}{" "}
+        </MapContext.Provider>
+      )}
     </div>
   );
 };
--- src/pages/map.js
+++ src/pages/map.js
@@ -9,10 +9,10 @@ function MapPage() {
   const [data, setData] = useState(null);
   useEffect(() => {
     // TODO: Make this a static fetch during build-time?
-    fetch('https://csd-restaurants.herokuapp.com/geo_restaurants.geojson')
-    .then(response => response.json())
-    .then(response => setData(response))
-  }, [])
+    fetch("https://csd-restaurants.herokuapp.com/geo_restaurants.geojson")
+      .then(response => response.json())
+      .then(response => setData(response));
+  }, []);
 
   return (
     <Layout>
@@ -26,9 +26,17 @@ function MapPage() {
         ]}
         title="Map"
       />
-      <Map center={[-84.1916, 39.7589]} zoom={14}>
-        <GeoJsonRestaurants geojson={data} />
-      </Map>
+
+      <div>
+        <h2 className="text-lg font-semibold text-gray-800 py-2">
+          Map View <sup>(BETA)</sup>
+        </h2>
+        <p className="pb-2">The current map view is a work in progress</p>
+
+        <Map center={[-84.1916, 39.7589]} zoom={14}>
+          <GeoJsonRestaurants geojson={data} />
+        </Map>
+      </div>
     </Layout>
   );
 }
